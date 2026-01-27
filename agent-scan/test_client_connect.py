d5c6d68bdbbb8ca4714008b239fa61cf75dc9683
Truman <1071684343@qq.com>
2026-01-27 16:23:53 +0800

    feat(connect): add connectivity test client

---
+++ agent-scan/test_client_connect.py
@@ -0,0 +1,20 @@
+import argparse
+
+from utils.aig_logger import scanLogger
+from core.agent_adapter.adapter import AIProviderClient
+
+parser = argparse.ArgumentParser(description="Agent CLI Runner")
+parser.add_argument("--client_file", type=str, help="Client config file")
+parser.add_argument("--prompt", type=str, default="Only return 1", help="Client config file")
+args = parser.parse_args()
+
+def is_client_connect(file_path: str, prompt: str):
+    client = AIProviderClient()
+    agent_provider = client.load_config_from_file(file_path)[0]
+    response = client.call_provider(agent_provider, prompt)
+    scanLogger.result_update(content=response.model_dump())
+    return response
+
+
+if __name__ == "__main__":
+    res = is_client_connect(args.client_file, args.prompt)
